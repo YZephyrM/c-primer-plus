68467720a470c89af1a1220ce17a416f542d9c99
YZephyrM <yzmpietro@gmail.com>
2025-06-28 16:24:15 +0200

    .

---
+++ c-primer-plus/chapter4/longstrg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	printf("Here's one way to print a ");	// method 1 split it in various pieces
+	printf("ling string.\n");
+	printf("Here's another way to print a \
+	long string. \n");      // to terminate the end of the first line with a return combination
+	printf("Here's the newest way to print a "
+	"long string.\n");   // string concatenation
+	return 0;
+}
